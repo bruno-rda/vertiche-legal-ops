94f2d488ce7f76feec56fbb735c1d74f050ef709
bruno-rda <206255519+bruno-rda@users.noreply.github.com>
2026-06-08 19:29:11 -0600

    refactor: move frontend app into frontend directory

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,11 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  envDir: '..',
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
